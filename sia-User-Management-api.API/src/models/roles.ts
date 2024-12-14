9441f06fddf1e48cc85fe31a1c824c2978e65ce5
Elvin Festin <vinfestinn@gmail.com>
2024-12-14 12:10:11 +0800

    change

--- sia-User-Management-api.API/src/models/roles.ts
+++ sia-User-Management-api.API/src/models/roles.ts
@@ -1,14 +1,18 @@
 // src/models/Roles.ts
 import mongoose, { Document, Schema } from 'mongoose';
 
-interface IRole extends Document {
-    roleID: string;
-    roleType: 'manager' | 'cashier' | 'guest_user';
+interface IRoles extends Document {
+    role_id: string;
+    manager: boolean;
+    cashier: boolean;
+    guess_user: boolean;
 }
 
-const RoleSchema = new Schema<IRole>({
-    roleID: { type: String, required: true, unique: true },
-    roleType: { type: String, enum: ['manager', 'cashier', 'guess_user'], required: true },
+const RoleSchema = new Schema<IRoles>({
+    role_id: { type: String, required: true, unique: true },
+    manager: { type: Boolean, required: true },
+    cashier: { type: Boolean, required: true },
+    guess_user: { type: Boolean, required: true },
 });
 
-export const Role = mongoose.model<IRole>('Role', RoleSchema);
+export const Role = mongoose.model<IRoles>('Role', RoleSchema);
