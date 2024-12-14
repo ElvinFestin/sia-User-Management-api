483565468a665af914ac356cf4622451c3123a1c
Elvin Festin <vinfestinn@gmail.com>
2024-12-14 12:02:13 +0800

    minor change

--- sia-User-Management-api.API/src/models/roles.ts
+++ sia-User-Management-api.API/src/models/roles.ts
@@ -3,13 +3,11 @@ import mongoose, { Document, Schema } from 'mongoose';
 
 interface IRole extends Document {
     roleID: string;
-    userID: string;
     roleType: 'manager' | 'cashier' | 'guest_user';
 }
 
 const RoleSchema = new Schema<IRole>({
     roleID: { type: String, required: true, unique: true },
-    userID: { type: String, ref: 'User', required: true },
     roleType: { type: String, enum: ['manager', 'cashier', 'guess_user'], required: true },
 });
 
